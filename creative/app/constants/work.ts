50b75d1c1d04d11cc7a743a5ccc3b0762bfdc89a
tvrcv <71032946+tvrcv@users.noreply.github.com>
2025-05-14 20:14:43 +0200

    changing Work Experience

--- creative/app/constants/work.ts
+++ creative/app/constants/work.ts
@@ -4,37 +4,31 @@ import { WorkTimelinePoint } from "../types";
 export const WORK_TIMELINE: WorkTimelinePoint[] = [
   {
     point: new THREE.Vector3(0, 0, 0),
-    year: '2014',
-    title: 'IIT Roorkee',
-    subtitle: 'Electrical Engineering',
+    year: '2019',
+    title: 'Virginia Polytechnic Institute and State University',
+    subtitle: 'B.S in Industrial and Systems Engineering',
     position: 'right',
   },
   {
     point: new THREE.Vector3(-4, -4, -3),
-    year: '2016',
-    title: 'XPrep',
-    subtitle: 'Frontend Intern',
+    year: '2021',
+    title: 'SENER',
+    subtitle: 'Industrial Engineering Intern',
     position: 'left',
   },
   {
     point: new THREE.Vector3(-3, -1, -6),
-    year: '2017',
-    title: 'Headout',
-    subtitle: 'Software Developer Intern',
+    year: '2021',
+    title: 'Cisco Systems',
+    subtitle: 'Technical Consultant Engineer',
     position: 'left',
   },
   {
     point: new THREE.Vector3(0, -1, -10),
-    year: '2018',
-    title: 'Cohesity',
-    subtitle: 'Member of Technical Staff',
+    year: '2023',
+    title: 'Oliver Wyman  Group',
+    subtitle: 'Business Relationship Analyst',
     position: 'left',
   },
-  {
-    point: new THREE.Vector3(1, 1, -12),
-    year: new Date().toLocaleDateString('default', { year: 'numeric' }),
-    title: '?',
-    subtitle: '???',
-    position: 'right',
-  }
+
 ]
